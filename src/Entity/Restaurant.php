09b31467aa3cee9192fea61659f7fb8fe81b6d52
Lucas <estebeweb@gmail.com>
2020-03-16 14:31:07 +0100

    Relations Created

--- src/Entity/Restaurant.php
+++ src/Entity/Restaurant.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -30,6 +32,28 @@ class Restaurant
      * @ORM\Column(type="datetime")
      */
     private $createdAt;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\City", inversedBy="restaurants")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $city;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\RestaurantPicture", mappedBy="restaurant", orphanRemoval=true)
+     */
+    private $restaurantPictures;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Review", mappedBy="restaurant", orphanRemoval=true)
+     */
+    private $reviews;
+
+    public function __construct()
+    {
+        $this->restaurantPictures = new ArrayCollection();
+        $this->reviews = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -68,6 +92,80 @@ class Restaurant
     public function setCreatedAt(\DateTimeInterface $createdAt): self
     {
         $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getCity(): ?City
+    {
+        return $this->city;
+    }
+
+    public function setCity(?City $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|RestaurantPicture[]
+     */
+    public function getRestaurantPictures(): Collection
+    {
+        return $this->restaurantPictures;
+    }
+
+    public function addRestaurantPicture(RestaurantPicture $restaurantPicture): self
+    {
+        if (!$this->restaurantPictures->contains($restaurantPicture)) {
+            $this->restaurantPictures[] = $restaurantPicture;
+            $restaurantPicture->setRestaurant($this);
+        }
+
+        return $this;
+    }
+
+    public function removeRestaurantPicture(RestaurantPicture $restaurantPicture): self
+    {
+        if ($this->restaurantPictures->contains($restaurantPicture)) {
+            $this->restaurantPictures->removeElement($restaurantPicture);
+            // set the owning side to null (unless already changed)
+            if ($restaurantPicture->getRestaurant() === $this) {
+                $restaurantPicture->setRestaurant(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Review[]
+     */
+    public function getReviews(): Collection
+    {
+        return $this->reviews;
+    }
+
+    public function addReview(Review $review): self
+    {
+        if (!$this->reviews->contains($review)) {
+            $this->reviews[] = $review;
+            $review->setRestaurant($this);
+        }
+
+        return $this;
+    }
+
+    public function removeReview(Review $review): self
+    {
+        if ($this->reviews->contains($review)) {
+            $this->reviews->removeElement($review);
+            // set the owning side to null (unless already changed)
+            if ($review->getRestaurant() === $this) {
+                $review->setRestaurant(null);
+            }
+        }
 
         return $this;
     }
